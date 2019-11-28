1e46e14275c84113023e8b130eb431dcd3c7a9c2
Scott Wolford <sawolford@gmail.com>
2019-11-27 22:54:13 -0500

    added guestrepo2.onsub.py helper script

---
+++ scripts/guestrepo2onsub.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import re, sys
+
+if len(sys.argv) != 2:
+    print("Usage: {0} <folder>".format(sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
+    pass
+folder = sys.argv[1]
+hgguestrepo = "{folder}/.hgguestrepo".format(folder=folder)
+hggrmapping = "{folder}/.hggrmapping".format(folder=folder)
+
+tokre = re.compile('=| ')
+def tokenize(lines):
+    global tokre
+    rv = []
+    for line in lines:
+        tokens = tokre.split(line[:-1])
+        tokens = [tok for tok in tokens if len(tok) > 0]
+        rv.append(tokens)
+        continue
+    return rv
+
+guestrepotok = tokenize(open(hgguestrepo).readlines())
+grmappingtok = tokenize(open(hggrmapping).readlines())
+
+guestrepo = {}
+for name, sym, rev in guestrepotok:
+    guestrepo[name] = (sym, rev)
+    continue
+
+grmapping = {}
+for name, repo in grmappingtok:
+    grmapping[name] = repo
+    continue
+
+print("hg = [")
+for name, (sym, rev) in guestrepo.items():
+    repo = grmapping[name]
+    print('    ("', end="")
+    print(name, end="")
+    print('", "', end="")
+    print(repo, end="")
+    print('", "', end="")
+    print(rev, end="")
+    print('"),')
+    continue
+print("]")
